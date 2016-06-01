9c14d8124caef509684d7aa036bfce2ffe4920cb
maxahn <ahnmaximus@gmail.com>
2016-05-31 22:23:45 -0700

    didn't commit this earlier for some reason

--- lib/footman.rb
+++ lib/footman.rb
@@ -1,10 +1,7 @@
 # http://classic.battle.net/war3/human/units/footman.shtml
-
-class Footman
-
+require_relative 'unit'
+class Footman < Unit
   def initialize
-    # Need to default the 2 instance variables here
-    # Also also give code outside this class access to these variables (via attr_reader, attr_writer or attr_accessor)
+    super(60,10) 
   end
-
 end
